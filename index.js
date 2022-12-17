01cb912c3cc9415c48ceed53eef367650cf97c19
Rebecca Wollard <rbwollard@gmail.com>
2022-12-16 20:43:06 -0700

    commit tree 41939275c5fc2fcb510840cf12d4c486c9646de5

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
+    if (distance <= 400) {
+    return `This one is on me!`} 
+      else if (distance >400 && distance <2000) {
+      return `That will be twenty bucks.`} 
+      else if (distance >2000 && distance <2500) {
+      return `I will gladly take your thirty bucks.`} 
+        else if (distance >2500) {
+        return `No can do.`
+      }
+    }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+scuberGreetingForFeet(199);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// function ternaryCheckCity(place){
+// //   const place = NYC 
+//     NYC ? `Ok, sounds good.` : `No go.`;
+// // }
+// // console.log(ternaryCheckCity(ATL))
+
+// let city = NYC;
+// let ternaryCheckCity;
+
+// if (ternaryCheckCity === NYC) {
+//   result `OK, sounds good.`; } {
+//   } else {
+//     result `No go.`;
+//   }
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? `Ok, sounds good.` : `No go.`;
+  }
+
+
+
+function switchOnCharmFromTip(amtNum){
+  switch(amtNum){
+    case `generous`:
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
 }
+};
+
